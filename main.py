aa634840ec092d95b424c325a028e606f339063e
Max <makskajn86@gmail.com>
2023-05-17 22:04:06 +0300

    Version 0.1

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 """ Файл index.html находится, поскольку указана папка шаблонов """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 
 def index():
     """ функция обрабатывает шаблон index.html и возвращает получившийся документ"""
     return render_template('index.html')
+
+def develop():
+    return render_template('develop.html')
+
+def finance():
+    return render_template('finance.html')
 
 folder = os.getcwd() # запомнили текущую рабочую папку
 # Создаём объект веб-приложения:
@@ -15,6 +21,9 @@ app = Flask(__name__, template_folder=folder, static_folder=folder) # первы
 
 # создаём правило для URL '/': 
 app.add_url_rule('/', 'index', index)
+app.add_url_rule('/develop', 'develop', develop)
+app.add_url_rule('/finance', 'finance', finance)
+
 
 # Запускаем веб-сервер:
 app.run()
